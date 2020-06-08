7a0193ad242eafd4a63275c38ea2eb2db81f410e
adaezeodurukwe <adaeze.od@softcom.ng>
2020-06-08 19:28:44 +0100

    minor changes

--- src/config/baseUrl.js
+++ src/config/baseUrl.js
@@ -1,1 +1,1 @@
-export default window.location.href.includes('localhost') ? 'http://localhost:9000/api/v1' : 'https://safehaven-backend.herokuapp.com/api/v1';
+export default 'https://safehaven-backend.herokuapp.com/api/v1';
